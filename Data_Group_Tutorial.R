80aeae93a54d9869ecaaddfb792a7d044f906c6c
Corentin Gibert <corentingibert@gmail.com>
2020-12-03 14:35:07 +0100

    Add files via upload

---
+++ Data_Group_Tutorial.R
@@ -0,0 +1,16 @@
+#' Group vector made of 2 groups of assemblages
+#'
+#' Data from paleontological datasets: marine invertebrate from paleozoic
+#' @docType data
+#' @usage  data(Data_Group_Tutorial)
+#' @format An object of class data.frame()
+#' @keywords datasets, matrix
+#' @references In prep.
+#'
+#' @examples
+#' data(Data_Group_Tutorial)
+#' data(Data_Matrix_Tutorial)
+#' PerSIMPER(Matrix, Group)
+#'
+
+"Group"
